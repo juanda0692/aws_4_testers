df3598993f4a62ec655fa631bcb38fd313d70da0
juan-garces <juan.garces@globant.com>
2025-10-24 10:53:38 -0500

    Add ClimaLambda function

---
+++ src/Lambdas/ClimaLambda.ts
@@ -0,0 +1,30 @@
+import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
+import { config } from "dotenv";
+config();
+
+const lambda = new LambdaClient({ region: process.env.AWS_REGION });
+
+const invokeLambda = async () => {
+    const functionName: string = "Clima";
+    const payload: string = JSON.stringify({ key: "value" });
+    const command = new InvokeCommand({
+        FunctionName: functionName,
+        Payload: Buffer.from(payload),
+        InvocationType: "RequestResponse",
+    });
+
+    try {
+        const response = await lambda.send(command);
+        const responsePayload = JSON.parse(Buffer.from(response.Payload as Uint8Array).toString());
+        console.log("Respuesta del Lambda: ", responsePayload);
+        if (responsePayload.errorMessage) {
+            console.error("Error en la ejecución del Lambda: ", responsePayload.errorMessage);
+        } else {
+            console.log("Ejecución exitosa del Lambda: ", responsePayload);
+        }
+    } catch (e) {
+        console.error("Error al invocar el Lambda: ", e);
+    }
+}
+
+invokeLambda();
